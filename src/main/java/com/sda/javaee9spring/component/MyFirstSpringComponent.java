64ca1b3b4ec6f5bcd37d3023e80db4b76c04d012
Lyanne <hirly2@gmail.com>
2022-01-30 10:46:38 +0200

    [change] - examples of creating spring beans

---
+++ src/main/java/com/sda/javaee9spring/component/MyFirstSpringComponent.java
@@ -0,0 +1,7 @@
+package com.sda.javaee9spring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstSpringComponent {
+}
--- src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
+++ src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sda.javaee9spring;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,7 +17,33 @@ class Javaee9SpringApplicationTests {
         Child john = new Child("John", plasticGun);
         Parent jason = new Parent("Jason", john);
     }
+    @Test
+    void recordsTest(){
+        Animal animal = new Animal();
+        System.out.println(animal);
+
+        Toy plasticGun = new Toy("plastic gun");
+        System.out.println(plasticGun);
+    }
+    @Test
+    void equalsTest(){
+        Assertions.assertTrue(4 == 4);
+
+        Animal one = new Animal();
+        Animal two = new Animal();
+        Animal three = one;
+
+        Assertions.assertFalse(one == two);
+        Assertions.assertFalse(one.equals(two));
+
+
+        Toy plasticGun = new Toy("plastic gun");
+        Toy plasticGun2 = new Toy("plastic gun");
+        Assertions.assertEquals(plasticGun, plasticGun2);
+
+    }
 }
 record Toy(String name){}
 record Child(String name, Toy favouriteToy) {}
 record Parent(String name, Child child) {}
+class Animal{}
